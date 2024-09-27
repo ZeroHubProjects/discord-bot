2bccd8c1145c199500d8b32658ef7b31f43ca0aa
Rufus Zero <rufus.nero.m@gmail.com>
2024-09-27 14:28:17 +0300

    feat: throttle sending users to verification

--- internal/discord/verification/user.go
+++ internal/discord/verification/user.go
@@ -2,11 +2,21 @@ package verification
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
+const cooldown = time.Minute
+
+// map of userID to last time they were sent to verification
+var lastUserVerificationSend = map[string]time.Time{}
+
 func (h *ByondVerificationHandler) SendUserToVerification(userID string) error {
+	// check if user was recently sent to verification to avoid spamming
+	if t, ok := lastUserVerificationSend[userID]; ok && !time.Now().UTC().After(t.Add(cooldown)) {
+		return nil
+	}
 	allow := int64(discordgo.PermissionViewChannel)
 	deny := int64(0)
 	permType := discordgo.PermissionOverwriteTypeMember
@@ -23,5 +33,6 @@ func (h *ByondVerificationHandler) SendUserToVerification(userID string) error {
 	if err != nil {
 		return fmt.Errorf("failed to delete ping: %w", err)
 	}
+	lastUserVerificationSend[userID] = time.Now().UTC()
 	return nil
 }
